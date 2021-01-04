1768ecc8c51a0ba005f42819a7efe47df11408e9
hafsah <emekomahafsah@gmail.com>
2021-01-04 18:06:34 +0100

    style: changes to colour scheme

--- src/PageBody.js
+++ src/PageBody.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react'
 import BookSection from './BookSection.js'
 
 
-
 class PageBody extends Component {
   render(){
     const bookList = this.props.bookList
     const currentlyReading = bookList.filter(book => book.shelf === 'currentlyReading');
     const wantToRead = bookList.filter(book => book.shelf === 'wantToRead');
     const read = bookList.filter(book => book.shelf === 'read');
+    
 
     return (
         <div className="list-books">
@@ -17,6 +17,7 @@ class PageBody extends Component {
         </div>
 
         <div className='list-books-content'>
+
            <BookSection 
             shelfName='Currently Reading' 
             books={currentlyReading}
@@ -34,6 +35,7 @@ class PageBody extends Component {
             books={read}
             moveBook={this.props.moveBook}
             />
+            
         </div>
       </div>
     )
